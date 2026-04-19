841c7a787aa6351e06ef9b87313d6c9e0ae29495
Michael Koren [Sinq1] <Miki801+sinq1@gmail.com>
2026-04-20 02:56:47 +0300

    android: bump app version to 2.0.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.axolync.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.8.0-beta.1"
+        versionCode = 20
+        versionName = "2.0.0-beta.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         aaptOptions {
             ignoreAssetsPattern = "!.svn:!.git:!.ds_store:!*.scc:.*:!CVS:!thumbs.db:!picasa.ini:!*~"
