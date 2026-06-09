ae892b7a34e77f5dd073e054f7b9c07c9447cd8a
Michael Koren [Sinq1] <Miki801+sinq1@gmail.com>
2026-06-09 07:03:05 +0300

    chore: bump version to 2.2.0

--- wrappers/mobile/capacitor/android/app/build.gradle.kts
+++ wrappers/mobile/capacitor/android/app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.axolync.android"
         minSdk = 24
         targetSdk = 34
-        versionCode = 21
-        versionName = "2.1.0"
+        versionCode = 22
+        versionName = "2.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         aaptOptions {
             ignoreAssetsPattern = "!.svn:!.git:!.ds_store:!*.scc:.*:!CVS:!thumbs.db:!picasa.ini:!*~"
